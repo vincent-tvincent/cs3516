e1b9c50a118028fe59fd7178e34faeb7432077e2
Yuancen Pu <vincent.pu@outlook.com>
2023-12-03 00:55:26 -0500

    rdt2.0

--- student2.c
+++ student2.c
@@ -37,18 +37,74 @@
  * in-order, and correctly, to the receiving side upper layer.
  */
 
+
+struct msg dequeue(struct message_queue** queue_start,unsigned int ack){
+  struct message_queue *queue = *queue_start;
+  while(!queue->is_message){queue = queue->next;}
+  struct msg next_message = queue->message;
+  if(ack){queue = queue->next;};
+  return next_message;
+}
+
+void enqueue(struct message_queue** queue_end,struct msg message){
+  struct message_queue *queue = *queue_end;
+  struct message_queue next_queue_end = {
+    .is_message = 1,
+    .message = message,
+    .next = NULL
+  };
+  queue->next = &next_queue_end;
+  queue = queue->next;
+}
+
+// generate the check sum 
+unsigned int generate_check_sum(char* vdata, int acknum, int seqnum) {
+  int i, checksum = 0;
+  for(i = 0; i < MESSAGE_LENGTH; i++){
+    checksum += (int)(vdata[i]) * i;
+  }
+  checksum += acknum * 21;
+  checksum += seqnum * 22;
+  return checksum;
+}
+
+// get current sequence number 
+int get_sequence_num(int AorB) {
+  int sequence_num;
+  if (AorB == AEntity){
+    sequence_num = A_sequence_num;
+    A_sequence_num = !A_sequence_num;
+  }else{
+    sequence_num = B_sequence_num;
+    B_sequence_num = !B_sequence_num; 
+  }
+  return sequence_num;
+}
+
+// send out a package 
+void send(int AorB,int ack_num, int seq_num, int check_sum, char *content) {
+  struct pkt packet_to_send = {
+    .acknum = ack_num,
+    .checksum = generate_check_sum(content, ack_num, seq_num),
+    .seqnum = seq_num
+  };
+  strncpy(packet_to_send.payload, content, MESSAGE_LENGTH);
+  tolayer3(AorB,packet_to_send);
+}
+
+// accept the incoming package and send to layer 5 
+void accept(int AorB, char *content) {
+  struct msg message_accepted = {}; 
+  strncpy(message_accepted.data, content, MESSAGE_LENGTH);
+  tolayer5(AorB,message_accepted);
+}
+
 void A_output(struct msg message) {
-  // struct pkt packet_to_B =  {
-  //   .acknum = ack,
-  //   .checksum = generate_check_sum(message.data, packet_to_B.acknum,packet_to_B.seqnum),
-  //   .seqnum = 0 
-  // };
-  // A_sequence_num = !A_sequence_num;
-  // strncpy(packet_to_B.payload, message.data,MESSAGE_LENGTH);
-  // tolayer3(AEntity,packet_to_B);  
-  char * content = message.data;
-  unsigned int check_sum = generate_check_sum(content, ack, get_sequence_num(AEntity));
-  send(AEntity, ack, get_sequence_num(AEntity), check_sum, content);
+  enqueue(&A_message_queue_end,message); 
+  struct msg next_message = dequeue(&A_message_queue_start,A_ack);
+
+  unsigned int check_sum = generate_check_sum(next_message.data, ack, get_sequence_num(AEntity));
+  send(AEntity, ack, get_sequence_num(AEntity), check_sum, next_message.data); 
 }
 
 /*
@@ -56,7 +112,7 @@ void A_output(struct msg message) {
  * implementation is bi-directional.
  */
 void B_output(struct msg message)  {
-  
+
 }
 
 /* 
@@ -66,7 +122,7 @@ void B_output(struct msg message)  {
  * packet is the (possibly corrupted) packet sent from the B-side.
  */
 void A_input(struct pkt packet) {
-
+  A_ack = packet.acknum;
 }
 
 /*
@@ -82,6 +138,13 @@ void A_timerinterrupt() {
 /* The following routine will be called once (only) before any other    */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init() {
+  struct message_queue queue = {
+    .is_message = 0
+  };
+  A_message_queue_start = &queue;
+  A_message_queue_end = A_message_queue_start;
+  A_sequence_num = 0;
+  A_ack = 1;
 }
 
 
@@ -95,15 +158,18 @@ void A_init() {
  * of a tolayer3() being done by a A-side procedure) arrives at the B-side. 
  * packet is the (possibly corrupted) packet sent from the A-side.
  */
-void B_input(struct pkt packet) {
-  struct msg message_from_A = {};
-  if(packet.acknum){
-    strncpy(message_from_A.data,packet.payload,MESSAGE_LENGTH);
-    tolayer5(BEntity,message_from_A);
+void B_input(struct pkt packet) { 
+  int income_check_sum = packet.checksum;
+  int income_ack_num = packet.acknum;
+  char *income_content = packet.payload;
+  int income_sequence_num = packet.seqnum; 
+  int real_check_sum = generate_check_sum(income_content, income_ack_num, income_sequence_num);
+  if(income_check_sum - real_check_sum == 0){
+    accept(BEntity, packet.payload); 
+    send(BEntity,ack,0,0,"");
   }else{
-         
+    send(BEntity,nack,0,0,"");
   }
-     
 }
 
 /*
@@ -114,52 +180,17 @@ void B_input(struct pkt packet) {
  */
 void  B_timerinterrupt() {
 }
-
 /* 
  * The following routine will be called once (only) before any other   
  * entity B routines are called. You can use it to do any initialization 
  */
 void B_init() {
-}
-
-
-unsigned int generate_check_sum(char* vdata, int acknum, int seqnum){
-  int i, checksum = 0;
-  for(i = 0; i < MESSAGE_LENGTH; i++){
-    checksum += (int)(vdata[i]) * i;
-  }
-  checksum += acknum * 21;
-  checksum += seqnum * 22;
-  return checksum;
-}
-
-// send out a package 
-void send(int AorB,int ack_num, int seq_num, int check_sum, char *content){
-  struct pkt packet_to_send = {
-    .acknum = ack_num,
-    .checksum = generate_check_sum(content, ack_num, seq_num),
-    .seqnum = seq_num
+  struct message_queue queue = {
+    .is_message = 0
   };
-  strncpy(packet_to_send.payload, content, MESSAGE_LENGTH);
-  tolayer3(AorB,packet_to_send);
+  B_message_queue_start = &queue;
+  B_message_queue_end = A_message_queue_start;
+  B_sequence_num = 0;
+  B_ack = 1;
 }
 
-// accept the incoming package and send to layer 5 
-void accept(int AorB, char *content){
-  struct msg message_accepted = {}; 
-  strncpy(message_accepted.data, content, MESSAGE_LENGTH);
-  tolayer5(AorB,message_accepted);
-}
-
-// get current sequence number 
-int get_sequence_num(int AorB){
-  int sequence_num = -1;
-  if (AEntity){
-    sequence_num = A_sequence_num;
-    A_sequence_num = !A_sequence_num;
-  }else if (BEntity){
-    sequence_num = B_sequence_num;
-    B_sequence_num = !B_sequence_num; 
-  }
-  return sequence_num;
-}
